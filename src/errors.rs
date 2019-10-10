c5834380238668d2c55b66be3b6be5509d5f629f
Prasanna Loganathar <pvl@prasannavl.com>
2019-10-11 03:07:44 +0530

    expose more error helpers

--- src/errors.rs
+++ src/errors.rs
@@ -7,17 +7,26 @@ where
     }
 }
 
-fn print_error_chain(e: &dyn std::error::Error) {
-    print_error(e, None, None);
+pub fn print_error_chain(e: &dyn std::error::Error) {
+    print_error_chain_with_prefix(e, None, Some("Caused By: "), None);
+}
+
+pub fn print_error_chain_with_prefix(
+    e: &dyn std::error::Error,
+    err_prefix: Option<&str>,
+    err_cause_prefix: Option<&str>,
+    backtrace_prefix: Option<&str>,
+) {
+    print_error(e, err_prefix, backtrace_prefix);
     let mut cause = e.source();
     while let Some(e) = cause {
-        print_error(e, Some("Caused by: "), None);
+        print_error(e, err_cause_prefix, backtrace_prefix);
         cause = e.source();
     }
 }
 
 #[allow(unused_variables)]
-fn print_error(
+pub fn print_error(
     e: &dyn std::error::Error,
     err_prefix: Option<&str>,
     backtrace_prefix: Option<&str>,
