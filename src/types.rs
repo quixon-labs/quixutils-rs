3e35074f4efd7cc1db9e9709bc44c933803bdcb9
Prasanna Loganathar <pvl@prasannavl.com>
2019-10-11 04:12:54 +0530

    fix typo

--- src/types.rs
+++ src/types.rs
@@ -1,7 +1,7 @@
 use anyhow::Error;
 use std::result::Result as StdResult;
 
-/// Convenience alias for result with `failure::Error`
+/// Convenience alias for result with `anyhow::Error`
 /// as default when E isn't provided. Fully compatible
 /// with std Result when E is provided, and is a drop-in
 /// replacement.
