459623accaf8f1ec1537594cc3eca38ae73672b7
Prasanna Loganathar <pvl@prasannavl.com>
2019-10-11 04:59:23 +0530

    simplify error helpers

--- src/errors.rs
+++ src/errors.rs
@@ -8,19 +8,18 @@ where
 }
 
 pub fn print_error_chain(e: &dyn std::error::Error) {
-    print_error_chain_with_prefix(e, None, Some("Caused By: "), None);
+    print_error_chain_with_prefix(e, None, Some("Caused By: "));
 }
 
 pub fn print_error_chain_with_prefix(
     e: &dyn std::error::Error,
     err_prefix: Option<&str>,
     err_cause_prefix: Option<&str>,
-    backtrace_prefix: Option<&str>,
 ) {
-    print_error(e, err_prefix, backtrace_prefix);
+    print_error(e, err_prefix);
     let mut cause = e.source();
     while let Some(e) = cause {
-        print_error(e, err_cause_prefix, backtrace_prefix);
+        print_error(e, err_cause_prefix);
         cause = e.source();
     }
 }
@@ -29,16 +28,14 @@ pub fn print_error_chain_with_prefix(
 pub fn print_error(
     e: &dyn std::error::Error,
     err_prefix: Option<&str>,
-    backtrace_prefix: Option<&str>,
 ) {
     let err_prefix = err_prefix.unwrap_or("Error: ");
     eprintln!("{}{}", err_prefix, e);
 
     #[cfg(feature = "unstable")]
     {
-        let bt_prefix = backtrace_prefix.unwrap_or("Backtrace:\n");
         if let Some(b) = e.backtrace() {
-            eprintln!("{}{}", bt_prefix, b);
+            eprintln!("{}", b);
         }
     }
 }
